98870842e1d25400807ed5d5db5576b1d747c029
Malarz pl <Malarz-pl@fake.wikipedia.org>
2019-01-30 23:52:17 +0000

    dyskografie standardowo

--- src/CzyWiesz.js
+++ src/CzyWiesz.js
@@ -100,11 +100,6 @@ if (mw.config.get('wgNamespaceNumber') === 0) {
 				type  : 'talk',
 			},
 			{
-				label : 'Dyskografie',
-				page  : 'Dyskusja wikiprojektu:Dyskografie',
-				type  : 'talk',
-			},
-			{
 				label : 'Fizyka',
 				page  : 'Dyskusja wikiprojektu:Fizyka',
 				type  : 'talk',
